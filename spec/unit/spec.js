dfc371def4a33f5877693d3972863ce5b8c52b1b
Matt Puchlerz <matt@puchlerz.com>
2010-02-27 14:32:45 -0600

    Began implementing oo.js class stuff.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,8 +1,38 @@
+describe 'DesignApp'
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
-    end
+  it 'should exist'
+    DesignApp.should_not.be_null
   end
+  
+  it 'should have a list of tools'
+    DesignApp.tools.should.be_an Array
+  end
+  
+  it 'should be able to initialize'
+    DesignApp.initialize.should.be_a Function
+  end
+  
+  describe 'initializing'
+    
+    
+  end
+  
+  describe 'DesignApp.Tool'
+    
+    it 'should exist'
+      DesignApp.Tool.should_not.be_null
+    end
+    
+    describe 'DesignApp.Tool.Selector'
+      
+      it 'should extend DesignApp.Tool'
+        selector = new DesignApp.Tool.Selector()
+        selector.should.be_an_instance_of DesignApp.Tool.Selector
+        selector.should.be_an_instance_of DesignApp.Tool
+      end
+      
+    end
+    
+  end
+
 end
